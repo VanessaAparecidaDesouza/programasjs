caf69beef57b3ea20ac0069e022e00828818a8a6
VANESSA APARECIDA DE SOUZA <1090481911047@fatec.local>
2019-08-27 11:02:28 -0300

    exe 22

--- Funcoes.js
+++ Funcoes.js
@@ -1,99 +1,152 @@
 function exe1(){
-    // recupera os dados do usario
-    var nota1 = Number(document.getElementById("nota1").value);
-    var nota2 = Number(document.getElementById("nota2").value);
-    var nota3 = Number(document.getElementById("nota3").value);
-    var nota4 = Number(document.getElementById("nota4").value);
-    //processamento
-    //calculo media
+    nota1 = Number(document.getElementById("nota1").value);
+    nota2 = Number(document.getElementById("nota2").value);
+    nota3 = Number(document.getElementById("nota3").value);
+    nota4 = Number(document.getElementById("nota4").value);
+
     var media = (nota1+nota2+nota3+nota4)/4
-    //verifica aprovação
-    if (media>= 7){
-        alert("Aprovado com media "+ media)
-        console.log("Aprovadocom media " +media)
-    }    
-    else { 
-    alert("Reprovado com media " +media)
-    console.log("Reprovado com media " + media)
+
+    if (media>=7){
+        alert("APROVADO com a média " + media )
+        console.log("APROVADO com a média " + media )
+        
+    }
+    else {
+        alert("REPROVADO com a Média " + media)
+        console.log("REPROVADO com a média " + media )
+        
     }
 }
 function exe2(){
-    // recupera os dados do usario
-    var n1 = Number(document.getElementById("n1").value);
-    var n2 = Number(document.getElementById("n2").value);
-    
-    //processamento
-    //calculo media
-    var media = (n1+n2)/4
-    //verifica aprovação
-    if (media>= 7){
-        alert("Aprovado com media "+ media)
-}
-else {    
-    alert("Reprovado com media " +media)
+    n1 = Number(document.getElementById("n1").value);
+    n2 = Number(document.getElementById("n2").value);
+    var media = (n1 + n2)/2
+    if (media >= 7){
+        alert("APROVADO com a média " + media )
+        console.log("APROVADO com a média " + media )
+        
     }
-}
+    else if ((media >= 3) && (media < 7)){
+        alert("EXAME com a Média " + media)
+        console.log("EXAME com a média " + media )
+
+    }
+    else {
+        alert("REPROVADO com a Média " + media)
+        console.log("REPROVADO com a média " + media )
+    }
+}   
 function exe3(){
-    // recupera os dados do usario
-    var nro1 = Number(document.getElementById("nro1").value);
-    var nro2 = Number(document.getElementById("nro2").value);
-    
-    //verificar o menor 
-    if(nro1 < nro2){ 
-        alert("Menor "+ nro1)
-}
-else if (nro2 < nro1){    
-    alert("Menor " + nro2)
+    numero1 = Number(document.getElementById("numero1").value);
+    numero2 = Number(document.getElementById("numero2").value);
+
+    if (numero1 > numero2){
+        alert("O numero 2 é o menor numero: " + numero2)
     }
-else{ 
-    alert("Não existe menor ")
-  }
+    else if (numero2 > numero1){
+        alert("O numero 1 é o menor numero: " + numero1)
+    }
+    else{
+        alert("Os numeros sao iguais")
+    }
 }
 function exe4(){
-    // recupera os dados do usario
-    var numero1 = Number(document.getElementById("numero1").value);
-    var numero2 = Number(document.getElementById("numero2").value);
-    var numero3 = Number(document.getElementById("numero3").value);
-    //verificar o menor 
-    if((numero1 == numero2) && (numero2 == numero3)) {       
-    alert("Não existe maior")
-}
-    else if (numero1 >= numero2) && (numero1 >= numero3)){    
-    alert("Maior " + numero1)
-    else if (numero2 >= numero3)}
-    alert("Maior" + numero2
+    num1 = Number(document.getElementById("num1").value);
+    num2 = Number(document.getElementById("num2").value);
+    num3 = Number(document.getElementById("num3").value);
+
+    if ((num1 > num2) && (num2 > num3)){
+        alert("Maior numero: " + num1)
     }
-else{ 
-    alert("Maior " + numero3)
-  }
+    else if ((num2 > num1) && (num2 > num3 )){
+        alert("Maior numero: " + num2)
+    }
+    else if ((num1 == num2) && (num2 == num3 )){
+        alert(" Os numeros são iguais numero: " + num1)
+    }
+    else if ((num3 > num1) && (num3 > num2 )){
+        alert("Maior numero: " + num3)
+    }
+    else if ((num1 == num2) && (num2 > num3 )){
+        alert("Maior numero: " + num2)
+    }
+    else if ((num2 == num3) && (num2 > num1 )){
+        alert("Maior numero: " + num2)
+    }
+    else if ((num2 == num3) && (num1 > num2 )){
+        alert("Maior numero: " + num1)
+    }
+     else if ((num1 == num3) && (num2 > num3 )){
+        alert("Maior numero: " + num2)
+    }
+     else if ((num1 == num3) && (num1 > num2 )){
+        alert("Maior numero: " + num1)
+    }
 }
-function exe5(){
-      // recupera os dados do usario
-    var no1 = Number(document.getElementById("no1").value);
-    var no2 = Number(document.getElementById("no2").value);
-    var no3 = Number(document.getElementById("no3").value);
-    var resultado
-    switch(opcao){ 
-    case 1:resultado = (no1 + no2) / 2
-        break //não entra no case debaixo
-    case 2: if (no1 >= no2) / 2
-        break // resultado = no1 - no2
+ function exe5(){
+    nume1 = Number(document.getElementById("nume1").value);
+    nume2 = Number(document.getElementById("nume2").value);
+    opcao = Number(document.getElementById("opcao").value);
+    var resultado 
+    switch(opcao){
+        case 1: resultado = (nume1 + nume2)/2
+            break
+        case 2: if (nume1 >= nume2){
+            resultado = nume1 - nume2
+        }
+            else{
+                resultado = nume2 - nume1
+        }
+            break
+        case 3: resultado = nume1 * nume2
+            break
+        case 4: if (nume2 !=0){
+            resultado = nume1 / nume2
+        }
+        else
+            resultado = "A divisão por zero (0)"
+           break
+    default: resultado = "opção invalida"
+    }
+alert("resultado " + resultado)
+    }
+function exe7(){
+    salario = Number(document.getElementById("salario").value)
+    if ( salario < 500){
+        novo = salario + salario*30/100
+        alert("novo valor " + novo)           
+    }
+    else {
+        alert ("Não tem direito ao aumento")
+    }
+
+} 
+function exe22(){
+    var idade = Number(document.getElementById("idade").value)
+    var peso = Number(document.getElementById("peso").value)
+    var risco = 0
+    if ( idade < 20){ 
+        if (peso <60){
+        risco = 9    
+        }
+      else if (peso <=90){
+        risco = 8  
+    }
+     else {
+         risco = 7        
+    }
+    }
+    else if (idade < 50){
+        if (peso <= 60){
+            risco = 6
+        }
+        else if(peso <=90){
+            risco = 5
         }
         else { 
-            resultado = no1 / no2
-        }
-        break //não entra no case debaixo 
-    case 3:resultado = no1 * no2 
-        break //não entra no case debaixo
-    case 4:if (no2 ! = 0){ 
-        resultado = no1/ no2
-        }
-        else { 
-            resultado = "Divisao por zero"
-        }
-        break //não entra no case debaixo 
-        default:resultado = "Opção invalido"
+            risco = 4
+     }
+        alert("Risco" + risco)
+        
 }
-alert("Resultado " + resultado)
-  
-    
+} 
